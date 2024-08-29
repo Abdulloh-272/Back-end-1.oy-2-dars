44bd3260d54e81ba620b3a62d32c50bb979aed8c
Abdulloh-272 <167763951+Abdulloh-272@users.noreply.github.com>
2024-08-29 17:02:58 +0500

    Add files via upload

---
+++ main.py
@@ -0,0 +1,26 @@
+
+multfilm = 'Dasha :)'
+footballer = "Cristiano Ronaldo"
+mars = "Kosmosdaligi"
+trip = "Dubai"
+cars = 2
+brand = 'BMW F90 COMPETITION'
+
+print(f'Men yomon korgan multfilm {multfilm}')
+print(f'Eng yaxshi futbolchi {footballer}')
+print(f'Marsni yoqadigan tomoni {mars}')
+print(f'Sayohat qilmoqchi bogan davlat {trip}')
+print(f'Moshina soni {cars}')
+print(f'Manga yoqadigan moshina brendi {brand}')
+
+
+
+number1 = int(11.2)
+number2 = str(33)
+number3 = str(90.90)
+number4 = int(1212.12)
+number5 = int(8899)
+
+print(f'int: {number1} str: {number2} str: {number3} int: {number4} int: {number5}')
+
+
